be3d94c631c01b35e78433529b6ae32e46c13bac
Dawid <Kurek>
2022-12-12 17:29:15 +0100

    add mobile view

--- index.js
+++ index.js
@@ -38,3 +38,19 @@ const handleRightClick = () => {
     activeIndex = nextIndex;
   });
 };
+
+/* -- Mobile Nav Toggle -- */
+
+const nav = document.querySelector("nav");
+
+const handleNavToggle = () => {
+  nav.dataset.transitionable = "true";
+
+  nav.dataset.toggled = nav.dataset.toggled === "true" ? "false" : "true";
+};
+
+window.matchMedia("(max-width: 800px)").onchange = (e) => {
+  nav.dataset.transitionable = "false";
+
+  nav.dataset.toggled = "false";
+};
